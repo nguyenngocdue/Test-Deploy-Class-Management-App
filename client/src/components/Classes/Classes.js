aa45997a2e01b9e2321fc7805cd79399037ee3bb
NguyenNgocDue <duengocnguyen@gmail.com>
2022-03-01 14:08:17 +0700

    test heroku

---
+++ client/src/components/Classes/Classes.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import AppNavbar from "../AppNavbar";
+import { useSelector } from "react-redux";
+import LinkButton from '../../components/linkButton/LinkButton';
+
+/* Button */
+import { Button } from 'react-bootstrap';
+
+const Classes = () => {
+  const { classes } = useSelector((state) => state.cla);
+  const classList = classes.map((c) => c.class_name.toUpperCase());
+
+  return (
+    <div className="container" style={{top : "25%"}}>
+      <div className="student__wrapper">
+        <AppNavbar />
+
+        <div className="classes__wrapper">
+          {classes.length > 0 ? (
+            <ul className="allClasses">
+              {classes.map(({ class_name, slug }, id) => {
+                return (
+                  <li key={id}>
+                    {/* {console.log(class_name, "slug:",slug)} */}
+                    <Link to={`/about-class/${slug}`}>
+                      {class_name.toUpperCase()}
+                    </Link>
+                  </li>
+                );
+              })}
+              <div>
+                <Button ariant="primary"><Link to="/create-class" style={{zindex : "999"}} >Create a class</Link></Button>
+              </div>
+            </ul>
+          ) : (
+            <div>
+              <h3 style={{color : "red"}}>No Class Available</h3>
+              <br />
+              <Button ariant="primary">
+                <Link to="/create-class">Create a class</Link>
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Classes;
---
+++ config/db.js
@@ -0,0 +1,31 @@
+const mysql = require("mysql");
+require('dotenv/config');
+
+var db;
+connectDatabase = () => {
+  if (!db) {
+    db = mysql.createConnection({
+      host: process.env.DBHOST,
+      database: process.env.DBNAME,
+      user: process.env.DBUSER,
+      password:  process.env.DB_PASS,
+      // host: "us-cdbr-east-05.cleardb.net",
+      // user: "b7bafd4bb9f161",
+      // password:  "c12b8433",
+      // database: "heroku_5d1510ed175ce44",
+    });
+    db.connect(function (err) {
+      if (!err) {
+        console.log("Database is connected!");
+      } else {
+        console.log("Error connecting database!");
+      }
+    });
+  }
+  return db;
+};
+module.exports = connectDatabase();
+
+
+// vast-ridge-96173
+//mysql://baf95f51e8e853:271e1603@us-cdbr-east-05.cleardb.net/heroku_056284627c926ae?reconnect=true
